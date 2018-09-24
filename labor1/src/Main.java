cee0e42577d1baab09b49fc7fc24dc96303e1e04
bagifruzsina <43538537+bagifruzsina@users.noreply.github.com>
2018-09-24 16:14:08 +0200

    Add files via upload

---
+++ labor1/src/Main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author hallgato
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        Hallgató h1= new Hallgató("Bőrönd Ödön","ASD123",1);
+        Hallgató h2= new Hallgató("Deb Ella","ZTG999",1);
+        h1.köszön(h2);
+    }
+}
